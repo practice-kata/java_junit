fc4dda87d156e8beca97f1d74f5b3e322d2fb0ce
d.kanai <d.kanai@pastoraldog.com>
2018-08-26 12:04:27 +0900

    add junit

---
+++ src/test/java/UserSignatureTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserSignatureTest {
+
+    @Test
+    void validate(){
+        assertEquals(true, true);
+    }
+}
